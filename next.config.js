e716dbcc28b06ac799e177af1ea1410c66417a38
Mohdcb <mohdsaraahmd12@gmail.com>
2025-03-09 23:54:15 +0530

    Initial commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', 'delivery-six-psi.vercel.app','finanace-backend.onrender.com'],
-  },
-}
-
-module.exports = nextConfig 
